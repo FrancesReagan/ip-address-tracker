1c633206049e91e270a5ffe0098639ac4045e311
Frances Reagan James <adtgwen@gmail.com>
2025-06-04 10:18:14 -0400

    added personal API_KEY from geoipify but grayed out until ready to deploy

--- src/secrets.js
+++ src/secrets.js
@@ -1,4 +1,5 @@
 
 // export const API_KEY = ""
 export const API_KEY = "my-api-key"
-//but api key here// don't push to git hub
+//but my personal api key here Your personal API KEY:
+// at_e0zF6yMJ7oxQNMqonRNWXKFaLf27q// don't push to git hub
