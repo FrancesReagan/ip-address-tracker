0ceb6f5d0ef295cb1eebbc580e3fdec7282cc968
Frances Reagan James <adtgwen@gmail.com>
2025-06-04 10:30:39 -0400

    added global variables for application and added function to initialize map

--- src/script.js
+++ src/script.js
@@ -20,4 +20,24 @@ fetch(url)
 .then(data => console.log(data))
 
 
-// 
+// my Global Variables that need to be available and modifiable by any function in application
+// throughout its repeated runs/lifecycle//
+
+// this holds the leaflet map instance //
+let map;  
+
+//this holds the leaflet marker instance (for IP's location)//
+let marker;
+
+// Function to Initialize Map-----this function will set up the leaflet  map and the attempts to locate user//
+  function initMap() {
+    // initialize map on the "map" div//
+    // the locate method is called instantly to get the user's true geo location position.//
+    // and the setView:true will instantly zoom the map to the location found //
+    // and the maxZoom: 16 sets max zoom level//
+    map = L.map("map").locate({setView:true, maxZoom:16});
+
+
+
+    
+  }
--- src/secrets.js
+++ src/secrets.js
@@ -2,4 +2,4 @@
 // export const API_KEY = ""
 export const API_KEY = "my-api-key"
 //but my personal api key here Your personal API KEY:
-// at_e0zF6yMJ7oxQNMqonRNWXKFaLf27q// don't push to git hub
+// at_e0zF6yMJ7oxQNMqonRNWXKFaLf27q  from geoipify.org 1000 fetches// don't push to git hub
