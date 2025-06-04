477f8f930f3d344e9cc4febd08774f873b0eecb8
Frances Reagan James <adtgwen@gmail.com>
2025-06-04 11:30:16 -0400

    added more explanation to code so that in future I can go back and recall what everything is and the logic behind it

--- src/script.js
+++ src/script.js
@@ -34,7 +34,10 @@ let marker;
     // initialize map on the "map" div//
     // the locate method is called instantly to get the user's true geo location position.//
     // and the setView:true will instantly zoom the map to the location found //
-    // and the maxZoom: 16 sets max zoom level//
+    // and the maxZoom: 16 sets max zoom level---and "L" is code marker reference
+    // to the Leaflet library---open source JavaScript libary for maps--so L is leaflet's global
+    // namespace object that contains all of its functions and classes---so L.marker is calling
+    // leaflet's marker functin to create a map marker//
     map = L.map("map").locate({setView:true, maxZoom:16});
 
 // add the base OpenStreetMap layer to map--this give the visual background for the map.//
@@ -44,9 +47,24 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   .addTo(map);
   // add zoom control to map for better user interactions---and place on the bottom-rt//
   L.control.zoom({
-    position:'bottomright' })
+    position:"bottomright" })
     .addTo(map);
-    
+
+    // custom marker icon---this uses leaflet divIcon to style the maker with pure CSS//
+    const customIcon = L.divIcon({
+      // this css class will be styled in styles.css//
+      className: "custom-marker", 
+      // width and height of icon marker//
+      iconSize: [46,56],
+      // this is point of icon that corresponds to the marker's location of bottom-center//
+      iconAnchor: [23,56],
+
+    });
+
+    //initialize marker---place it at [0,0] initially as placeholder. Real position
+    // will be updated dynamically when I or user fetches the IP location data.//
+    marker = L.marker([0,0], {icon: customIcon})", {})
+
   })
 })
 
